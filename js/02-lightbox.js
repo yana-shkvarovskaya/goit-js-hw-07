0b24a8b1a21e18371b916a4f9c27f596fdddd202
yana-shkvarovskaya <shkvarovskayayana@gmail.com>
2021-10-14 15:58:14 +0300

    добавляет 2 задание

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryImgsMarkup = createGalleryImgMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryImgsMarkup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function createGalleryImgMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href=${original}>
+        <img class="gallery__image" src=${preview} alt=${description} />
+        </a> 
+      `;
+    })
+    .join("");
+}
