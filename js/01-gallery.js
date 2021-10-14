dda98291b471ce0f7572c4474d2c24732ba84ab6
yana-shkvarovskaya <shkvarovskayayana@gmail.com>
2021-10-14 15:58:32 +0300

    форматирует

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,19 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
-const galleryContainer = document.querySelector('.gallery');
+const galleryContainer = document.querySelector(".gallery");
 const galleryImgsMarkup = createGalleryImgMarkup(galleryItems);
 
-galleryContainer.insertAdjacentHTML('beforeend', galleryImgsMarkup);
+galleryContainer.insertAdjacentHTML("beforeend", galleryImgsMarkup);
 
-galleryContainer.addEventListener('click', onGalleryContainerClick);
+galleryContainer.addEventListener("click", onGalleryContainerClick);
 
 function createGalleryImgMarkup(galleryItems) {
-    return galleryItems
-      .map(({ preview, original, description }) => {
-        return `
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
         <div class="gallery__item">
         <a class="gallery__link" 
         href= ${original}>
@@ -26,36 +26,35 @@ function createGalleryImgMarkup(galleryItems) {
         </a>
       </div>
       `;
-      })
-      .join('');
-  }
-
+    })
+    .join("");
+}
 
 function onGalleryContainerClick(event) {
-    event.preventDefault();
-const isImgSelected = event.target.classList.contains('gallery__image');
-  
-if (!isImgSelected) {
-      return;
-}
+  event.preventDefault();
+  const isImgSelected = event.target.classList.contains("gallery__image");
 
-let selectedImg = event.target.dataset.source;
+  if (!isImgSelected) {
+    return;
+  }
 
-const instance = basicLightbox.create(`
+  let selectedImg = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
     <img src="${selectedImg}" width="800" height="600">
-`)
+`);
 
-instance.show()
+  instance.show();
 
-if(instance.visible){
-    document.addEventListener('keydown', onEscKeyPress);
+  if (instance.visible) {
+    document.addEventListener("keydown", onEscKeyPress);
 
     function onEscKeyPress(event) {
-        const ESC_KEY_CODE = 'Escape';
-  
-      if(event.code === ESC_KEY_CODE){
-          instance.close()
-      };
-}
-}
+      const ESC_KEY_CODE = "Escape";
+
+      if (event.code === ESC_KEY_CODE) {
+        instance.close();
+      }
+    }
   }
+}
